54f4bb75f9cdb6b3578fe2c079eb75a225176f06
Mojtaba Michael <mojimich2015@gmail.com>
2024-11-07 08:18:24 -0800

    Added kafka producer

--- data_receiver/main.go
+++ data_receiver/main.go
@@ -5,11 +5,39 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/confluentinc/confluent-kafka-go/kafka"
 	"github.com/gorilla/websocket"
 	"github.com/mjmichael73/toll-calculator/types"
 )
 
+const kafkaTopic = "obudata"
+
 func main() {
+	p, err := kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": "localhost"})
+	if err != nil {
+		panic(err)
+	}
+	defer p.Close()
+	go func() {
+		for e := range p.Events() {
+			switch ev := e.(type) {
+			case *kafka.Message:
+				if ev.TopicPartition.Error != nil {
+					fmt.Printf("Delivery failed: %v\n", ev.TopicPartition)
+				} else {
+					fmt.Printf("Delivered message to %v\n", ev.TopicPartition)
+				}
+			}
+		}
+	}()
+	topic := kafkaTopic
+	for i := 0; i < 10; i++ {
+		p.Produce(&kafka.Message{
+			TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+			Value: []byte("test producing "),
+		}, nil)
+	}
+	return
 	recv := NewDataReceiver()
 	http.HandleFunc("/ws", recv.handleWS)
 	http.ListenAndServe(":30000", nil)
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/mjmichael73/toll-calculator
 go 1.23.2
 
 require (
+	github.com/confluentinc/confluent-kafka-go v1.9.2 // indirect
 	github.com/confluentinc/confluent-kafka-go/v2 v2.6.0 // indirect
 	github.com/gorilla/websocket v1.5.3 // indirect
 )
