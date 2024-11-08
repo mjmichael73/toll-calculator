d1be4c9f1c067d6a57f94f87ca04647fd9fca542
Mojtaba Michael <mojimich2015@gmail.com>
2024-11-08 02:50:36 -0800

    Refactored Kafka producer

---
+++ data_receiver/producer.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/mjmichael73/toll-calculator/types"
+)
+
+type DataProducer interface {
+	produceData(types.OBUData) error
+}
+
+type KafkaProducer struct {
+	producer *kafka.Producer
+}
+
+func NewKafkaProducer() (*KafkaProducer, error) {
+	p, err := kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": "localhost"})
+	if err != nil {
+		return nil, err
+	}
+	go func() {
+		for e := range p.Events() {
+			switch ev := e.(type) {
+			case *kafka.Message:
+				if ev.TopicPartition.Error != nil {
+					fmt.Printf("Delivery failed: %v\n", ev.TopicPartition)
+				} else {
+					fmt.Printf("Delivered message to %v\n", ev.TopicPartition)
+				}
+			}
+		}
+	}()
+	return &KafkaProducer{
+		producer: p,
+	}, nil
+}
+
+func (p *KafkaProducer) produceData(data types.OBUData) error {
+	b, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+	return p.producer.Produce(&kafka.Message{
+		TopicPartition: kafka.TopicPartition{
+			Topic:     &kafkaTopic,
+			Partition: kafka.PartitionAny,
+		},
+		Value: b,
+	}, nil)
+}
